4dab5efa247768d44e9030fe53551a86ac2c0ef7
kayesmith94 <50887040+kayesmith94@users.noreply.github.com>
2019-11-27 11:34:49 -0500

    Remade the index, validated view model, todos 1-6.

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
 using TechJobs.ViewModels;
+using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
@@ -18,9 +19,12 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
+            // TODO #1 DONE -get the Job with the given ID and pass it into the view
+            //Get this screen after adding a job *hint*
+            //can add validation to make sure id is valid later just in case the user types something in
 
-            return View();
+            Job singleJob = jobData.Find(id);
+            return View(singleJob);
         }
 
         public IActionResult New()
@@ -32,9 +36,24 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
+            // TODO #6 DONE - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+
+            if(ModelState.IsValid)
+            {
+                //make job from new job
+                Job newJob = new Job {
+                    Name = newJobViewModel.Name,
+                    CoreCompetency = newJobViewModel.CoreCompetency,
+                    Employer = newJobViewModel.Employer,
+                    Location = newJobViewModel.Location,
+                    PositionType = newJobViewModel.PositionType
+                };
+                jobData.Jobs.Add(newJob);
+
+                return Redirect($"/job?id={newJob.ID}");
+            }
 
             return View(newJobViewModel);
         }
--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,8 +15,16 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
-        // TODO #3 - Included other fields needed to create a job,
-        // with correct validation attributes and display names.
+        public Employer Employer { get; set; }
+      
+        public Location Location { get; set; }
+        
+        public CoreCompetency CoreCompetency { get; set; }
+       
+        public PositionType PositionType { get; set; }
+
+
+        // TODO #3 DONE??? - Included other fields needed to create a job, with correct validation attributes and display names.
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
@@ -35,9 +43,36 @@ namespace TechJobs.ViewModels
                     Text = field.Value
                 });
             }
+            // TODO #4 DONE??? - populate the other List<SelectListItem> collections needed in the view
+            ///kaye start
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
 
-            // TODO #4 - populate the other List<SelectListItem> 
-            // collections needed in the view
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())//is it a problem that they're all "field"? maybe not b/c of scope
+
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
 
         }
     }
